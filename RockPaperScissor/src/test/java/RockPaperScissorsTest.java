f09b67b08203292340d6ca29a81d347cc01bfa76
nav1710 <nav1710@gmail.com>
2018-01-16 21:11:38 +0000

    Update RockPaperScissorsTest.java
    renamed service variable

--- RockPaperScissor/src/test/java/RockPaperScissorsTest.java
+++ RockPaperScissor/src/test/java/RockPaperScissorsTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.*;
 
 public class RockPaperScissorsTest
 {
-    private RockPaperScissorsService rockPaperScissorsService;
+    private RockPaperScissorsService service;
 
     private Player playerA, playerB;
 
@@ -23,19 +23,19 @@ public class RockPaperScissorsTest
         playerA = new Player("Jade", RuleEnum.ROCK);
         playerB = new Player("Jean", RuleEnum.SCISSOR);
 
-        rockPaperScissorsService = new RockPaperScissorsService(rounds, playerA, playerB);
-        rockPaperScissorsService.startGame();
+        service = new RockPaperScissorsService(rounds, playerA, playerB);
+        service.startGame();
 
         stringBuilder = new StringBuilder();
         stringBuilder.append(String.format("\"models.Player %s wins %d of %d games\"\n", playerA.getName(), playerA.getWins(), rounds));
         stringBuilder.append(String.format("\"models.Player %s wins %d of %d games\"\n", playerB.getName(), playerB.getWins(), rounds));
-        stringBuilder.append(String.format("\"Tie: %d of %d games\"", rockPaperScissorsService.getDrawRounds(), rounds));
+        stringBuilder.append(String.format("\"Tie: %d of %d games\"", service.getDrawRounds(), rounds));
     }
 
     @Test
     public void outputMessageTest() throws Exception
     {
-        String outputMessage = RockPaperScissors.outputMessage(rockPaperScissorsService, playerA, playerB);
+        String outputMessage = RockPaperScissors.outputMessage(service, playerA, playerB);
 
         assertThat(outputMessage, is(stringBuilder.toString()));
     }
